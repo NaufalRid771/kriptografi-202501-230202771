1fac905111710a2f8657672d9225379eef5cafa3
Naufal Raa'id <nraid834@gmail.com>
2025-10-21 12:48:08 +0700

    week2-cryptosystem

--- praktikum/week2-cryptosystem/src/code-example.py
+++ praktikum/week2-cryptosystem/src/code-example.py
@@ -21,7 +21,7 @@ def decrypt(ciphertext, key):
     return result
 
 if __name__ == "__main__":
-    message = "Nanda Erdi Pratama"
+    message = "Naufal Raaid"
     key = 5
 
     enc = encrypt(message, key)
