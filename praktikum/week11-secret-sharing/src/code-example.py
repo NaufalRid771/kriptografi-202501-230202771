dd3981b373d548ee8dc96155fba08ad6cba260f9
Naufal Raaid <nraid834@gmail.com>
2026-01-21 22:30:42 +0700

    week11-secret-sharing/src/code-example.py

--- praktikum/week11-secret-sharing/src/code-example.py
+++ praktikum/week11-secret-sharing/src/code-example.py
@@ -1,12 +1,9 @@
-import builtins
-builtins.long = int
-
 from secretsharing import SecretSharer
 
 # Rahasia yang ingin dibagi
 secret = "KriptografiUPB2025"
 
-# Bagi menjadi 5 shares, threshold 3
+# Bagi menjadi 5 shares, ambang batas 3 (minimal 3 shares untuk rekonstruksi)
 shares = SecretSharer.split_secret(secret, 3, 5)
 print("Shares:", shares)
 
